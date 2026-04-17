e494adb8b916d64afe6de2b0ea36576eb759ac7d
kelivreno <joikonkazama@gmail.com>
2026-04-16 20:58:52 -0700

    fix: remove unnecessary comments from index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// INITIALIZE the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect new button
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
